500f567c9e357832f6b03470f82e1acc9d6316e1
Glassware123 <anilbas@gmail.com>
2015-07-12 00:49:57 +0300

    module problem solved

---
+++ core/src/main/java/resources/java/com/xmobile/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package main.java.resources.java.com.xmobile.dao;
+
+/**
+ * Created by ubuntu on 12.07.2015.
+ */
+public class BaseDao {
+}
---
+++ core/src/main/resources/java/com/xmobile/domain/JMSInfrastructure.java
@@ -0,0 +1,8 @@
+package main.resources.java.com.xmobile.domain;
+
+/**
+ * Created by ubuntu on 12.07.2015.
+ */
+public class JMSInfrastructure {
+
+}
